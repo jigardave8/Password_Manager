d55706cbf44e6e07d17e084343588e9709e99dcb
Jay D <jdavenz8@gmail.com>
2023-09-30 11:34:33 +1300

    Initial Commit

---
+++ Password_Manager/Password_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_ManagerApp.swift
+//  Password_Manager
+//
+//  Created by Jigar on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Password_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
